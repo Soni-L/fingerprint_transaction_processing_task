2ee2335bc9520b06805540c2abc211992f9c76ea
soni <acdsoni@gmail.com>
2022-07-30 14:32:58 +0200

    json import

--- solution.js
+++ solution.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+// Let's first create a joint JSON array with all the transactions and their respective latencies 
+// included all in one place
+
+import transactions from "./transactions_data/transactions.json" assert { type: "json" };
+import latencies from "./transactions_data/latencies.json" assert { type: "json" };
+
+const all_transactions = transactions;
+console.log(transactions[0])
